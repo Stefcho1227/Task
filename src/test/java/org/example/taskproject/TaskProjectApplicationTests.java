0ee9346d5a10514e9e0551c5c4ebc65a7114aee2
Stefcho1227 <stefan.ivanov1227@gmail.com>
2025-03-04 10:11:00 +0200

    first commit

---
+++ src/test/java/org/example/taskproject/TaskProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.taskproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
